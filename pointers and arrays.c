7779bdaecd885099a374928f4eefca4aa86f990f
Mahudoom <66937573+easa2000@users.noreply.github.com>
2021-08-13 23:23:24 +0530

    Create pointers and arrays.c

---
+++ pointers and arrays.c
@@ -0,0 +1,11 @@
+pointers and array
+
+#include<stdio.h>
+main()
+{
+    int i=1;
+    int arr[] = {1,2,3,4,5};
+    printf("\n Address of array = %p",(i+arr));
+    // int *ptr = &arr[i];
+    // printf("\n Value of first element : %d",*ptr);
+}
